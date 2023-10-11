a609ffbadb715b773d832190b468d6e9261368ff
Gilang <143797853+GilangRK411@users.noreply.github.com>
2023-10-11 18:49:26 +0700

    Update tugas1stukdat..cpp

--- tugas1stukdat..cpp
+++ tugas1stukdat..cpp
@@ -3,62 +3,90 @@
 
 using namespace std;
 
-// Struktur data untuk menyimpan informasi mahasiswa
+const int MAX_MAHASISWA = 10;
+
 struct Mahasiswa {
     string nama;
     string nrp;
     int usia;
-    string jenisKelamin;
+    string jeniskelamin;
+    string hobi;
+    string semester;
+    string angkatan;
+    string jurusan;
+    string alamat;
+    string tempatlahir;
 };
 
-// Fungsi untuk menampilkan data mahasiswa
-void tampilkanData(const Mahasiswa mahasiswas[], int jumlahData) {
+void tampilkandatamahasiswa(const Mahasiswa mahasiswa[], int jumlahdata) {
     cout << "Data Mahasiswa:" << endl;
-    for (int i = 0; i < jumlahData; ++i) {
+    for (int i = 0; i < jumlahdata; ++i) {
         cout << "Mahasiswa ke-" << (i + 1) << endl;
-        cout << "NRP: " << mahasiswas[i].nrp << endl;
-        cout << "Nama: " << mahasiswas[i].nama << endl;
-        cout << "Usia: " << mahasiswas[i].usia << endl;
-        cout << "Jenis Kelamin: " << mahasiswas[i].jenisKelamin << endl;
+        cout << "NRP: " << mahasiswa[i].nrp << endl;
+        cout << "Nama: " << mahasiswa[i].nama << endl;
+        cout << "Usia: " << mahasiswa[i].usia << endl;
+        cout << "Jenis Kelamin: " << mahasiswa[i].jeniskelamin << endl;
+        cout << "Hobi: " << mahasiswa[i].hobi << endl;
+        cout << "Semester: " << mahasiswa[i].semester << endl;
+        cout << "Angkatan: " << mahasiswa[i].angkatan << endl;
+        cout << "Jurusan: " << mahasiswa[i].jurusan << endl;
+        cout << "Alamat: " << mahasiswa[i].alamat << endl;
+        cout << "Tempat Lahir: " << mahasiswa[i].tempatlahir << endl;
         cout << endl;
     }
 }
 
-// Fungsi untuk menambahkan data mahasiswa baru
-void tambahData(Mahasiswa mahasiswas[], int &jumlahData, int maksimalMahasiswa) {
-    if (jumlahData < maksimalMahasiswa) {
+void tambahdatamahasiswa(Mahasiswa mahasiswa[], int &jumlahdata, int maksimaldata) {
+    if (jumlahdata < maksimaldata) {
         cout << "Masukkan NRP Mahasiswa: ";
-        cin >> mahasiswas[jumlahData].nrp;
+        cin >> mahasiswa[jumlahdata].nrp;
         cout << "Masukkan Nama Mahasiswa: ";
         cin.ignore();
-        getline(cin, mahasiswas[jumlahData].nama);
+        getline(cin, mahasiswa[jumlahdata].nama);
         cout << "Masukkan Usia Mahasiswa: ";
-        cin >> mahasiswas[jumlahData].usia;
+        cin >> mahasiswa[jumlahdata].usia;
         cout << "Masukkan Jenis Kelamin Mahasiswa (L/P): ";
-        cin >> mahasiswas[jumlahData].jenisKelamin;
-        jumlahData++;
+        cin >> mahasiswa[jumlahdata].jeniskelamin;
+        cout << "Masukkan Hobi Mahasiswa: ";
+        cin.ignore();
+        getline(cin, mahasiswa[jumlahdata].hobi);
+        cout << "Masukkan Semester Mahasiswa: ";
+        cin.ignore();
+        getline(cin, mahasiswa[jumlahdata].semester);
+        cout << "Masukkan Angkatan Mahasiswa: ";
+        cin.ignore();
+        getline(cin, mahasiswa[jumlahdata].angkatan);
+        cout << "Masukkan Jurusan Mahasiswa: ";
+        cin.ignore();
+        getline(cin, mahasiswa[jumlahdata].jurusan);
+        cout << "Masukkan Alamat Mahasiswa: ";
+        cin.ignore();
+        getline(cin, mahasiswa[jumlahdata].alamat);
+        cout << "Masukkan Tempat Lahir Mahasiswa: ";
+        cin.ignore();
+        getline(cin, mahasiswa[jumlahdata].tempatlahir);
+        jumlahdata++;
         cout << "Data mahasiswa berhasil ditambahkan." << endl;
     } else {
         cout << "Batas maksimum data mahasiswa telah tercapai." << endl;
     }
 }
 
-// Fungsi untuk menghapus data mahasiswa
-void hapusData(Mahasiswa mahasiswas[], int &jumlahData) {
-    if (jumlahData == 0) {
+void hapusdatamahasiswa(Mahasiswa mahasiswa[], int &jumlahdata) {
+    if (jumlahdata == 0) {
         cout << "Tidak ada data mahasiswa yang tersimpan." << endl;
     } else {
-        cout << "Masukkan NRP Mahasiswa yang ingin dihapus: ";
-        string nrpHapus;
-        cin >> nrpHapus;
+        cout << "Masukkan NRP Mahasiswa yang ingin dihapus:";
+        string nrphapus;
+        cin >> nrphapus;
 
         bool ditemukan = false;
-        for (int i = 0; i < jumlahData; ++i) {
-            if (mahasiswas[i].nrp == nrpHapus) {
-                for (int j = i; j < jumlahData - 1; ++j) {
-                    mahasiswas[j] = mahasiswas[j + 1];
+        for (int i = 0; i < jumlahdata; ++i) {
+            if (mahasiswa[i].nrp == nrphapus) {
+                for (int j = i; j < jumlahdata - 1; ++j) {
+                    mahasiswa[j] = mahasiswa[j + 1];
                 }
-                jumlahData--;
+                jumlahdata--;
                 ditemukan = true;
                 cout << "Data mahasiswa berhasil dihapus." << endl;
                 break;
@@ -71,25 +99,42 @@ void hapusData(Mahasiswa mahasiswas[], int &jumlahData) {
     }
 }
 
-// Fungsi untuk mengubah data mahasiswa
-void ubahData(Mahasiswa mahasiswas[], int jumlahData) {
-    if (jumlahData == 0) {
+void ubahdatamahasiswa(Mahasiswa mahasiswa[], int jumlahdata) {
+    if (jumlahdata == 0) {
         cout << "Tidak ada data mahasiswa yang tersimpan." << endl;
     } else {
-        cout << "Masukkan NRP Mahasiswa yang ingin diubah: ";
-        string nrpUbah;
-        cin >> nrpUbah;
+        cout << "Masukkan NRP Mahasiswa yang ingin diubah:";
+        string nrpubah;
+        cin >> nrpubah;
 
         bool ditemukan = false;
-        for (int i = 0; i < jumlahData; ++i) {
-            if (mahasiswas[i].nrp == nrpUbah) {
+        for (int i = 0; i < jumlahdata; ++i) {
+            if (mahasiswa[i].nrp == nrpubah) {
                 cout << "Masukkan Nama Mahasiswa yang baru: ";
                 cin.ignore();
-                getline(cin, mahasiswas[i].nama);
+                getline(cin, mahasiswa[i].nama);
                 cout << "Masukkan Usia Mahasiswa yang baru: ";
-                cin >> mahasiswas[i].usia;
+                cin >> mahasiswa[i].usia;
                 cout << "Masukkan Jenis Kelamin Mahasiswa yang baru (L/P): ";
-                cin >> mahasiswas[i].jenisKelamin;
+                cin >> mahasiswa[i].jeniskelamin;
+                cout << "Masukkan Hobi Mahasiswa yang baru: ";
+                cin.ignore();
+                getline(cin, mahasiswa[i].hobi);
+                cout << "Masukkan Semester Mahasiswa yang baru: ";
+                cin.ignore();
+                getline(cin, mahasiswa[i].semester);
+                cout << "Masukkan Angkatan Mahasiswa yang baru: ";
+                cin.ignore();
+                getline(cin, mahasiswa[i].angkatan);
+                cout << "Masukkan Jurusan Mahasiswa yang baru: ";
+                cin.ignore();
+                getline(cin, mahasiswa[i].jurusan);
+                cout << "Masukkan Alamat Mahasiswa yang baru: ";
+                cin.ignore();
+                getline(cin, mahasiswa[i].alamat);
+                cout << "Masukkan Tempat Lahir Mahasiswa yang baru: ";
+                cin.ignore();
+                getline(cin, mahasiswa[i].tempatlahir);
                 ditemukan = true;
                 cout << "Data mahasiswa berhasil diubah." << endl;
                 break;
@@ -102,43 +147,42 @@ void ubahData(Mahasiswa mahasiswas[], int jumlahData) {
     }
 }
 
+int tampilkanmenu() {
+    cout << "Pilih operasi yang ingin dilakukan:" << endl;
+    cout << "1. Tampilkan Data" << endl;
+    cout << "2. Tambah Data Mahasiswa" << endl;
+    cout << "3. Hapus Data Mahasiswa" << endl;
+    cout << "4. Ubah Data Mahasiswa" << endl;
+    cout << "5. Keluar" << endl;
+
+    int pilihan;
+    cin >> pilihan;
+    return pilihan;
+}
+
 int main() {
-    const int maksimalMahasiswa = 10;
-    Mahasiswa mahasiswas[maksimalMahasiswa];
-    int jumlahData = 0;
+    Mahasiswa mahasiswa[MAX_MAHASISWA];
+    int jumlahdata = 0;
 
     while (true) {
-        cout << "Pilih operasi yang ingin dilakukan:" << endl;
-        cout << "1. Tampilkan Data" << endl;
-        cout << "2. Tambah Data Mahasiswa" << endl;
-        cout << "3. Hapus Data Mahasiswa" << endl;
-        cout << "4. Ubah Data Mahasiswa" << endl;
-        cout << "5. Keluar" << endl;
-
-        int pilihan;
-        cin >> pilihan;
+        int pilihan = tampilkanmenu();
 
         switch (pilihan) {
             case 1:
-                tampilkanData(mahasiswas, jumlahData);
+                tampilkandatamahasiswa(mahasiswa, jumlahdata);
                 break;
-
             case 2:
-                tambahData(mahasiswas, jumlahData, maksimalMahasiswa);
+                tambahdatamahasiswa(mahasiswa, jumlahdata, MAX_MAHASISWA);
                 break;
-
             case 3:
-                hapusData(mahasiswas, jumlahData);
+                hapusdatamahasiswa(mahasiswa, jumlahdata);
                 break;
-
             case 4:
-                ubahData(mahasiswas, jumlahData);
+                ubahdatamahasiswa(mahasiswa, jumlahdata);
                 break;
-
             case 5:
                 cout << "Program selesai." << endl;
                 return 0;
-
             default:
                 cout << "Pilihan tidak valid. Silakan coba lagi." << endl;
         }
